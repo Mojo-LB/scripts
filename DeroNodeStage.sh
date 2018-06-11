0608abcfa90189f5c46d9b2b86f3de81658a2217
Mojo <35627917+Mojo-LB@users.noreply.github.com>
2018-06-12 01:46:17 +0300

    Update DeroNodeStage.sh

--- DeroNodeStage.sh
+++ DeroNodeStage.sh
@@ -10,28 +10,21 @@ cp node-side-linux /atlantis/
 cp dero_linux_amd64/* /atlantis 
 
 ## Stage Testnet/Mainnet folder - Uncomment and adjust links accordingly
-#wget http://seeds.dero.io/testnet/blockchain/derod_database.db.zip
-#sudo apt install unzip -y
-#unzip derod_database.db.zip
-#mkdir /atlantis/testnet
-#cp derod_database.db /atlantis/testnet
+wget http://electromine.net/derod_database.db.tar.gz
+tar -xvzf derod_database.db.tar.gz
+mkdir /atlantis/testnet
+cp derod_database.db /atlantis/testnet
 
-### Stats Site Script
-touch /atlantis/stats.sh
-echo "HOST=\$(uname -n)" >> /atlantis/stats.sh
-echo "cd /atlantis" >> /atlantis/stats.sh
-echo "echo  {\\\"myDaemon\\\":\\\"http://127.0.0.1:30306\\\",\\\"myName\\\":\\\"\\\$HOST \\\"} > config.json" > /atlantis/stats.sh
-echo "/./atlantis/node-side-linux" >> /atlantis/stats.sh
+### Stats Site Config
+# Will use machine hostname, if you want change it after script is done
+echo  "{\"myDaemon\":\"http://127.0.0.1:30306\",\"myName\":\"$(uname -n)\"}" > /atlantis/config.json
 
 ### Setup Crontab for auto start
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
+crontab -l > mycron # will give a warning if cron not used before, ignore
 echo "@reboot cd /atlantis/ && screen -S daemon -d -m  /./atlantis/derod-linux-amd64 --testnet" >> mycron
-echo "@reboot cd /atlantis/ && screen -S stats -d -m sh stats.sh" >> mycron
-#install new cron file
+echo "@reboot cd /atlantis/ && screen -S stats -d -m /./atlantis/node-side-linux" >> mycron
 crontab mycron
 rm mycron
 
-### Reboot for changes to tae effect
+### Reboot for changes to take effect
 sudo shutdown -r now
